81f5ea2c4269d83ee7753a1c2b7b57d4becc5d05
Parenbz <parenbz@yandex.ru>
2022-04-14 19:11:15 +0300

    Not working implementation

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <ctime>
 #include <iomanip>
+#include <stdio.h>
 
 #include "QBDI.h"
 #include "ShadowMemory.h"
@@ -12,24 +13,20 @@ uint8_t *source(int n) {
     return (uint8_t *)malloc(n);
 }
 
-void sink(uint8_t *s, int n) {
+//s подаётся через rdi, n через rsi
+void sink(uint8_t *s, size_t n) {
     free(s);
 }
 
-void func(int n) {
+void func(size_t n) {
     uint8_t *tstr = source(n);
-    uint8_t x = tstr[n / 2];
-    uint8_t *str = (uint8_t *)malloc(n);
-    uint8_t *rd = (uint8_t *)malloc(n);
 
-    for (int i = 0; i < n; i++) {
-        if (rand() % 2) rd[i] = tstr[i]; else rd[i] = str[i];
-    }
+    uint8_t *x = (uint8_t *)malloc(1);
 
-    sink(rd, n);
-
-    free(tstr);
-    free(str);
+    x[0] = tstr[0];
+    
+    sink(tstr, n);
+    sink(x, 1);
 }
 
 QBDI::VMAction showInstruction(QBDI::VM *vm, QBDI::GPRState *gprState, QBDI::FPRState *fprState, void *data) {
@@ -42,12 +39,14 @@ QBDI::VMAction showInstruction(QBDI::VM *vm, QBDI::GPRState *gprState, QBDI::FPR
 }
 
 //Помечаем данные, выделенные в source
-QBDI::VMAction taintFromSource(QBDI::VM *vm, QBDI::GPRState *gprState, QBDI::FPRState *fprState, void *data) {
+QBDI::VMAction taintSource(QBDI::VM *vm, QBDI::GPRState *gprState, QBDI::FPRState *fprState, void *data) {
     const QBDI::InstAnalysis *instAnalysis = vm->getInstAnalysis((QBDI::AnalysisType)7);
 
     if (instAnalysis->isReturn) {
         for (int i = 0; i < gprState->rdi; i++) {
             sm->taintByte(gprState->rax + i, i + 1);
+            std::cout << "Tainted byte " << std::setbase(16) << gprState->rax + i << std::setbase(10) 
+                        << " with offset " << sm->checkByte(gprState->rax + i) - 1 << std::endl;
         }
     }
 
@@ -61,24 +60,49 @@ QBDI::VMAction taintPropagation(QBDI::VM *vm, QBDI::GPRState *gprState, QBDI::FP
     if (instAnalysis->numOperands == 2) {
         if (sm->checkRegister(instAnalysis->operands[1].regCtxIdx)) {
             sm->taintRegister(instAnalysis->operands[0].regCtxIdx, sm->checkRegister(instAnalysis->operands[0].regCtxIdx));
+            std::cout << "Taint register " << instAnalysis->operands[0].regName << " with offset " 
+                        << sm->checkRegister(instAnalysis->operands[0].regCtxIdx) - 1 << std::endl;
         } else {
             sm->freeRegister(instAnalysis->operands[0].regCtxIdx);
+            std::cout << "Freed register " << instAnalysis->operands[0].regName << std::endl;
         }
     } else if (instAnalysis->numOperands == 6) {
         if (instAnalysis->operands[1].type == QBDI::OPERAND_GPR) {
-            if (sm->checkByteRange(instAnalysis->operands[4].value, instAnalysis->operands[0].size)) {
+            if (sm->checkByteRange(gprState->rbp + instAnalysis->operands[4].value, instAnalysis->operands[0].size)) {
                 sm->taintRegister(instAnalysis->operands[0].regCtxIdx, 
-                                    sm->checkByteRange(instAnalysis->operands[4].value, instAnalysis->operands[0].size));
+                                    sm->checkByteRange(gprState->rbp + instAnalysis->operands[4].value, instAnalysis->operands[0].size));
+                std::cout << "Taint register " << instAnalysis->operands[0].regName << " with offset "
+                            << sm->checkRegister(instAnalysis->operands[0].regCtxIdx) - 1<< std::endl;
             } else {
                 sm->freeRegister(instAnalysis->operands[0].regCtxIdx);
+                std::cout << "Freed resgister " << instAnalysis->operands[0].regName << std::endl;
             }
         } else {
             if (sm->checkRegister(instAnalysis->operands[5].regCtxIdx)) {
-                sm->taintByteRange(instAnalysis->operands[3].value, instAnalysis->operands[5].size, 
+                sm->taintByteRange(gprState->rbp + instAnalysis->operands[3].value, instAnalysis->operands[5].size, 
                                     sm->checkRegister(instAnalysis->operands[5].regCtxIdx));
+                std::cout << "Taint byte range" << std::setbase(16) << gprState->rbp + instAnalysis->operands[3].value << std::setbase(10) << " size of "
+                            << (unsigned)instAnalysis->operands[5].size << " with offset "
+                            << sm->checkByteRange(gprState->rbp + instAnalysis->operands[3].value, instAnalysis->operands[5].size) - 1 << std::endl;
             } else {
-                sm->freeByteRange(instAnalysis->operands[3].value, instAnalysis->operands[5].size);
+                sm->freeByteRange(gprState->rbp + instAnalysis->operands[3].value, instAnalysis->operands[5].size);
+                std::cout << "Freed byte range " << std::setbase(16) << gprState->rbp + instAnalysis->operands[3].value << std::setbase(10) 
+                            << " size of " << (unsigned)instAnalysis->operands[5].size << std::endl;
             }
+        }
+    }
+
+    return QBDI::VMAction::CONTINUE;
+}
+
+QBDI::VMAction taintSink(QBDI::VM *vm, QBDI::GPRState *gprState, QBDI::FPRState *fprState, void *data) {
+    QBDI::rword address = gprState->rdi;
+    size_t size = gprState->rsi;
+
+    for (size_t i = 0; i < size; i++) {
+        if (sm->checkByte(address + i)) {
+            std::cout << "Caught byte " << std::setbase(16) << address + i << std::setbase(10) 
+                        << " with offset " << sm->checkByte(address + i) - 1 << std::endl;
         }
     }
 
@@ -88,9 +112,8 @@ QBDI::VMAction taintPropagation(QBDI::VM *vm, QBDI::GPRState *gprState, QBDI::FP
 const static size_t STACK_SIZE = 0x100000;
 
 int main(int argc, char **argv) {
-    srand(time(0));
 
-    int n = atoi(argv[1]);
+    size_t n = atoi(argv[1]);
 
     uint8_t *fakestack;
     QBDI::GPRState *state;
@@ -100,8 +123,10 @@ int main(int argc, char **argv) {
     QBDI::allocateVirtualStack(state, STACK_SIZE, &fakestack);
     vm->addCodeCB(QBDI::POSTINST, showInstruction, nullptr);
     vm->addMnemonicCB("mov*", QBDI::PREINST, taintPropagation, nullptr);
-    vm->addCodeRangeCB((QBDI::rword)source, (QBDI::rword)sink, QBDI::PREINST, taintFromSource, nullptr);
+    vm->addCodeRangeCB((QBDI::rword)source, (QBDI::rword)sink, QBDI::PREINST, taintSource, nullptr);
+    vm->addCodeAddrCB((QBDI::rword)sink, QBDI::PREINST, taintSink, nullptr);
     vm->addInstrumentedModuleFromAddr((QBDI::rword)func);
     vm->call(nullptr, (QBDI::rword)func, {(QBDI::rword)n});
     QBDI::alignedFree(fakestack);
+    delete sm;
 }
